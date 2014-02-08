3d012415224def1115d6f9b0acc6357fbfb861c5
root <root@ip-172-31-13-212.eu-west-1.compute.internal>
2014-02-08 21:12:04 +0000

    Fixat register-user sa att den funkar som test

--- register-user.php
+++ register-user.php
@@ -1,3 +1,9 @@
 <?php
-file_put( './info_get.txt', print_r($_GET, true) );
-file_put( './info.txt', print_r($_POST, true) );
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+file_put_contents( 'info_get.txt', print_r($_GET, true) );
+file_put_contents( 'info.txt', print_r($_POST, true) );
+
+echo "hurra";
+
