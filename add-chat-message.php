4e3b3628d286c306c387c05b8f9312e5855cf258
Ludvig Lejon <ludvig.lejon@aol.com>
2014-02-09 00:51:34 +0100

    add chat message php

---
+++ add-chat-message.php
@@ -0,0 +1,12 @@
+<?php
+include("/hackaton-server/dbinfo.php");
+
+$userid = intval($_GET["userid"]);
+$circleid = intval($_GET["circleid"]);
+$message = $_GET["message"];
+
+$mysqli = mysql( $db[ 'server' ], $db[ 'username' ], $db[ 'password' ], $db[ 'database' ] );
+
+$result = $mysqli->query($connection, "INSERT INTO circle_chat ('circle_id', 'user_id', 'message') VALUES ($circleid, $userid, $message)");
+
+echo "INSERT INTO circle_chat ('circle_id', 'user_id', 'message') VALUES ($circleid, $userid, $message)";
